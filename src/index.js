bb2dda08a4bdec59aa9366d6eb38117cc91ba41e
bandinandrejj <79991210922@yandex.ru>
2022-03-17 22:49:50 +0300

    Сделал задание №8

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom';
 import {StrictMode} from 'react';
-import App from './tutorial-7/App';
+import App from './tutorial-8/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './tutorial-7/index.css';
+import './tutorial-8/styles.css';
 import {BrowserRouter} from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
