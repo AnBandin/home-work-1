77efd6d26ec3a785dceaf5aa511225c7a3f83211
AnBandin <79991210922@yandex.ru>
2022-04-26 20:12:58 +0300

    Обновил 9 урок

--- src/tutorial-9/App.js
+++ src/tutorial-9/App.js
@@ -1,35 +1,52 @@
 import {useState} from 'react';
+import axios from "axios";
 
 function App() {
-  const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]);
 
-  const getUsers = async () => {
-    try {
-      const response = await fetch(
-        'https://62544efc19bc53e2347c5576.mockapi.io/user'
-      );
+    const getUsers = async () => {
+        const {data} = await axios.get('https://62544efc19bc53e2347c5576.mockapi.io/users');
+        setUsers(data);
+    };
 
-      if (!response.ok) {
-        throw `Ошибка сервера ${response.status}`;
-      }
-
-      setUsers(await response.json());
-    } catch (err) {
-      console.error(err);
-      setUsers([]);
+    const createUsers = () => {
+        axios.post('https://62544efc19bc53e2347c5576.mockapi.io/users', {
+            name: 'Andrei B.'
+        })
     }
-  };
 
-  return (
-    <div>
-      <ul>
-        {users.map((obj) => (
-          <li key={obj.id}>{obj.name}</li>
-        ))}
-      </ul>
-      <button onClick={getUsers}>Получить список пользователей</button>
-    </div>
-  );
+    const loadFile = () => {
+        const fileElem = document.querySelector('#file');
+        const file = fileElem.files[0];
+        console.log(fileElem.files)
+
+        const formData = new FormData();
+        formData.append('file', file);
+
+        axios.post('http://localhostl:9999', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            }
+        })
+    }
+
+    return (
+        <div>
+            <ul>
+                {users.map((obj) => (
+                    <li key={obj.id}>{obj.name}</li>
+                ))}
+            </ul>
+            <button onClick={getUsers}>Получить список пользователей</button>
+            <button onClick={createUsers}>Добавит меня</button>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <input type='file' id='file'/>
+            <button onClick={loadFile}>Загрузить</button>
+        </div>
+    );
 }
 
 export default App;
