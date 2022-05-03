4279b23187f8340fff7922f4d09cd753a7300680
AnBandin <79991210922@yandex.ru>
2022-05-03 23:41:27 +0300

    Сделал урок #10

--- src/tutorial-10/components/Search.js
+++ src/tutorial-10/components/Search.js
@@ -1,25 +1,49 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import axios from 'axios';
+import debounce from '../func';
+import useDebounce from '../func';
 
 const Search = ({setUser}) => {
   const [search, setSearch] = useState('');
   const [loading, setLoad] = useState(false);
+  const debouncedInputValue = useDebounce(search, 500);
 
-  const getUsers = async (e) => {
-    e.preventDefault();
-    setLoad(true);
+  useEffect(() => {
+    if (debouncedInputValue) {
+      getUsers();
+    }
+  }, [debouncedInputValue]);
+
+  useEffect(() => {
+    const {search} = window.location;
+    if (search.length) {
+      const user = search.split('=')[1];
+      getUsers(user);
+      setSearch(user);
+    }
+  }, []);
+
+  const getUsers = async (user = undefined) => {
     try {
-      const {data} = await axios.get(`https://api.github.com/users/${search}`);
+      const {data} = await axios.get(
+        `https://api.github.com/users/${user || search}`
+      );
       setUser(data);
     } catch (e) {
-      alert('Пользователь не найден');
+      setUser({});
       console.error(e.message);
     }
+  };
+
+  const getForm = (e) => {
+    e.preventDefault();
+    setLoad(true);
+    getUsers();
     setLoad(false);
   };
 
   return (
-    <form className="app-form" onSubmit={getUsers}>
+    <form className="app-form" onSubmit={getForm}>
       <input
         type="text"
         className="app-input"
